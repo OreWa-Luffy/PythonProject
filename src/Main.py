e92f0e6c96fcc39ada864e29f8aeb9f0c3e82af2
Logan <108277501+OreWa-Luffy@users.noreply.github.com>
2024-03-20 18:30:26 +0000

    Made a basic menu. And some calculator inputs.

--- src/Main.py
+++ src/Main.py
@@ -1,2 +1,58 @@
-
 print("Hello World")
+
+
+def backToMenu():
+    print("Back to menu")
+    menu()
+
+
+def menu():
+    print("Welcome to the menu")
+
+    print("Please choose from the following")
+
+    print("1. Add \n 2. Subtract \n 3. Multiply \n 4. Divide \n 5. Exit")
+
+    choice = input("")
+    if choice == "1":
+        add = input("Enter the first")
+        add = int(add)
+        add2 = input("Enter the second")
+        add2 = int(add2)
+        total = add + add2
+        print(total)
+        backToMenu()
+
+
+    elif choice == "2":
+        sub = input("Enter the first number")
+        sub = int(sub)
+        sub2 = input("Enter the second number")
+        sub2 = int(sub2)
+        total = sub - sub2
+        print(total)
+        backToMenu()
+
+    elif choice == 3:
+        multi = input("Enter the first number")
+        multi = int()
+        multi2 = input("Enter the second number")
+        multi2 = int(multi2)
+        total = multi * multi2
+        print(total)
+        backToMenu()
+
+    elif choice == 4:
+        divide = input("Enter the first number")
+        divide = int()
+        divide2 = input("Enter the second number")
+        divide2 = int(divide2)
+        total = divide / divide2
+        print(total)
+        backToMenu()
+
+    elif choice == 5:
+        print("Goodbye")
+
+
+menu()
