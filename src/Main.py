44ba044799425253f05ae23892108cff5aaefeef
Logan <108277501+OreWa-Luffy@users.noreply.github.com>
2024-03-22 21:53:28 +0000

    Removed previous command line menu.

--- src/Main.py
+++ src/Main.py
@@ -2,71 +2,15 @@ import tkinter as tk
 from tkinter import *
 
 
-def backToMenu():
-    print("Back to menu")
-    menu()
-
-
-# This is a basic command line menu for calculator. Once More is understood this will be on a gui. #
-# Uses a switch like if statement #
-
-
-def menu():
-    print("Welcome to the menu")
-
-    print("Please choose from the following")
-
-    print("1. Add \n 2. Subtract \n 3. Multiply \n 4. Divide \n 5. Exit")
-    choice = input("")
-    if choice == "1":
-        add = input("Enter the first")
-        add = int(add)
-        add2 = input("Enter the second")
-        add2 = int(add2)
-        total = add + add2
-        print(total)
-        backToMenu()
-
-
-    elif choice == "2":
-        sub = input("Enter the first number")
-        sub = int(sub)
-        sub2 = input("Enter the second number")
-        sub2 = int(sub2)
-        total = sub - sub2
-        print(total)
-        backToMenu()
-
-    elif choice == 3:
-        multi = input("Enter the first number")
-        multi = int()
-        multi2 = input("Enter the second number")
-        multi2 = int(multi2)
-        total = multi * multi2
-        print(total)
-        backToMenu()
-
-    elif choice == 4:
-        divide = input("Enter the first number")
-        divide = int()
-        divide2 = input("Enter the second number")
-        divide2 = int(divide2)
-        total = divide / divide2
-        print(total)
-        backToMenu()
-
-    elif choice == 5:
-        print("Goodbye")
+# Now moved to GUI #
 
 def Add():
     add1 = Submit1.get()
     add2 = Submit2.get()
     total = add1 + add2
     label.config(text=total)
+    button.config(state=DISABLED)
 
-
-
-# menu()
 
 # GUI things.
 m = tk.Tk()
@@ -81,4 +25,3 @@ Submit2 = Entry(m, width=25)
 Submit2.pack()
 
 m.mainloop()
-
