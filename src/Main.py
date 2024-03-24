6ba0f9e1c283598982d78a5330270555325f20ba
Logan <108277501+OreWa-Luffy@users.noreply.github.com>
2024-03-24 18:35:34 +0000

    Changed it so the button will only accept ints and the button will activate only when there's input.

--- src/Main.py
+++ src/Main.py
@@ -20,7 +20,16 @@ def Add():
     add2 = int(Submit2.get())
     total = add1 + add2
     label.config(text=total)
-    button.config(state=DISABLED)
+
+
+def validate_and_enable_button(*args):
+    if Submit1.get() and Submit2.get():
+        if Submit1.get().isdigit() and Submit2.get().isdigit():
+            button.config(state=NORMAL)
+        else:
+            button.config(state=tk.DISABLED)
+    else:
+        button.config(state=tk.DISABLED)
 
 
 # GUI things.
@@ -28,12 +37,18 @@ m = tk.Tk()
 m.title("Calculator")
 label = Label(m, text="Welcome to calculator")
 label.pack()
-button = tk.Button(m, text="ADD", width=25, command=Add)
+button = tk.Button(m, text="ADD", width=25, command=Add, state=DISABLED)
 button.pack()
 validate_cmd = m.register(validate_input)
 Submit1 = Entry(m, width=25, validate="key", validatecommand=(validate_cmd, '%P'))
 Submit1.pack()
 Submit2 = Entry(m, width=25, validate="key", validatecommand=(validate_cmd, '%P'))
 Submit2.pack()
+
+
+# Binding the validate_and_enable_button function to any change in the Entry fields
+
+Submit1.bind("<KeyRelease>", validate_and_enable_button)
+Submit2.bind("<KeyRelease>", validate_and_enable_button)
 
 m.mainloop()
