38c6947de6891e2ffa663866d31c24c8ceed009d
Logan <108277501+OreWa-Luffy@users.noreply.github.com>
2024-03-24 18:28:18 +0000

    Changed it so the input will only recognise ints.

--- src/Main.py
+++ src/Main.py
@@ -4,6 +4,17 @@ from tkinter import *
 
 # Now moved to GUI #
 
+
+def validate_input(new_value):
+    if new_value == '':
+        return True
+    try:
+        int(new_value)
+        return True
+    except ValueError:
+        return False
+
+
 def Add():
     add1 = int(Submit1.get())
     add2 = int(Submit2.get())
@@ -19,9 +30,10 @@ label = Label(m, text="Welcome to calculator")
 label.pack()
 button = tk.Button(m, text="ADD", width=25, command=Add)
 button.pack()
-Submit1 = Entry(m, width=25)
+validate_cmd = m.register(validate_input)
+Submit1 = Entry(m, width=25, validate="key", validatecommand=(validate_cmd, '%P'))
 Submit1.pack()
-Submit2 = Entry(m, width=25)
+Submit2 = Entry(m, width=25, validate="key", validatecommand=(validate_cmd, '%P'))
 Submit2.pack()
 
 m.mainloop()
