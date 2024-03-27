433ebbae7140f752d92e7b9442f7e29fede230f8
Logan <108277501+OreWa-Luffy@users.noreply.github.com>
2024-03-27 21:17:52 +0000

    Added dark light button

--- src/Main.py
+++ src/Main.py
@@ -74,26 +74,52 @@ def validate_and_enable_button(*args):
         divideButton.config(state=tk.DISABLED)
 
 
+# light and dark mode
+
+def light_dark():
+    global switch_value
+    if switch_value:
+        m.configure(bg="black")
+        addButton.config(fg="white", bg="black")
+        subButton.config(fg="white", bg="black")
+        multiButton.config(fg="white", bg="black")
+        divideButton.config(fg="white", bg="black")
+        label.config(fg="white", bg="black")
+        light_darkButton.config(fg="white", bg="black")
+        clearButton.config(fg="white", bg="black")
+        Submit1.config(fg="white", bg="black")
+        Submit2.config(fg="white", bg="black")
+
+        switch_value = False
+    else:
+        m.configure(bg="white")
+        addButton.config(fg="black", bg="white")
+        subButton.config(fg="black", bg="white")
+        multiButton.config(fg="black", bg="white")
+        divideButton.config(fg="black", bg="white")
+        label.config(fg="black", bg="white")
+        light_darkButton.config(fg="black", bg="white")
+        clearButton.config(fg="black", bg="white")
+        Submit1.config(fg="black", bg="white")
+        Submit2.config(fg="black", bg="white")
+        switch_value = True
+
+
+
+
+
 # GUI things.
 m = tk.Tk()
 m.title("Calculator")
 m.geometry("360x150")
 icon_path = "C:/Users/baydr/Pictures/Uni/redstone.png"
-m.configure(bg="black")
-
-# Attempts to apply image to icon photo.
-try:
-    icon_image = tk.PhotoImage(file=icon_path)
-
-    m.iconphoto(False, icon_image)
-
-except Exception as e:
-    print("Error with file path.", e)
+icon_image = tk.PhotoImage(file=icon_path)
+m.iconphoto(False, icon_image)
+switch_value = True
 
 # Timer stuff
 
 m.start_time = time.time()
-
 timerLabel = tk.Label(m, text="Timer: 0 seconds", font=14, fg="green", background="black", )
 timerLabel.grid(row=6, column=0, columnspan=2)
 
@@ -113,30 +139,31 @@ label = Label(m, text="Welcome to calculator", fg="white", bg="black")
 label.grid(row=0, column=0, columnspan=2)
 
 # AddButton stuff
-addButton = tk.Button(m, text="ADD", width=25, command=Add, state=DISABLED, activebackground="Green", fg="white", bg="black")
+addButton = tk.Button(m, text="ADD", width=25, command=Add, state=DISABLED, activebackground="Green")
 addButton.grid(row=1, column=0)
 # SubButton stuff
-subButton = tk.Button(m, text="SUBTRACT", width=25, command=subTract, state=DISABLED, activebackground="Green", fg="white", bg="black")
+subButton = tk.Button(m, text="SUBTRACT", width=25, command=subTract, state=DISABLED, activebackground="Green")
 subButton.grid(row=1, column=1)
 # MultiButton stuff
-multiButton = tk.Button(m, text="MULTIPLY", width=25, command=multiply, state=DISABLED, activebackground="Green", fg="white", bg="black")
+multiButton = tk.Button(m, text="MULTIPLY", width=25, command=multiply, state=DISABLED, activebackground="Green")
 multiButton.grid(row=2, column=0)
 # DivideButton stuff
-divideButton = tk.Button(m, text="DIVIDE", width=25, command=divide, state=DISABLED, activebackground="Green", fg="white", bg="black")
+divideButton = tk.Button(m, text="DIVIDE", width=25, command=divide, state=DISABLED, activebackground="Green")
 divideButton.grid(row=2, column=1)
 # light/dark mode button
-
+light_darkButton = tk.Button(m, text="LIGHT MODE", width=25, command=light_dark)
+light_darkButton.grid(row=5, column=1, columnspan=2)
 
 validate_cmd = m.register(validate_input)
 
 # Label stuff
-Submit1 = Entry(m, width=25, validate="key", validatecommand=(validate_cmd, '%P'), fg="white", bg="black")
+Submit1 = Entry(m, width=25, validate="key", validatecommand=(validate_cmd, '%P'))
 Submit1.grid(row=3, column=0, columnspan=2)
-Submit2 = Entry(m, width=25, validate="key", validatecommand=(validate_cmd, '%P'), fg="white", bg="black")
+Submit2 = Entry(m, width=25, validate="key", validatecommand=(validate_cmd, '%P'))
 Submit2.grid(row=4, column=0, columnspan=2)
 
 # ClearButton stuff
-clearButton = tk.Button(m, text="CLEAR", width=15, command=clear, activebackground="Red", fg="white", bg="black")
+clearButton = tk.Button(m, text="CLEAR", width=15, command=clear, activebackground="Red")
 clearButton.grid(row=5, column=0, columnspan=2)
 
 # Binding the validate_and_enable_button function to any change in the Entry fields
