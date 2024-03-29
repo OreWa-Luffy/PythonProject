5c363706980c5fb341708887e4b8c578f9146be7
Logan <108277501+OreWa-Luffy@users.noreply.github.com>
2024-03-29 12:54:12 +0000

    Started work a new file for main menu.

---
+++ src/MainMenu.py
@@ -0,0 +1,11 @@
+# Going to be added to main. Just establishes main menu
+
+import tkinter as tk
+from tkinter import *
+import time
+
+mainMenu = tk.Tk()
+mainMenu.title("Main menu")
+mainMenu.geometry("400x200")
+mainMenu.resizable()
+mainMenu.mainloop()
