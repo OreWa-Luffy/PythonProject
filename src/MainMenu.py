99b54470771c4ee3d7770b548336ffd1dc5637f2
Logan <108277501+OreWa-Luffy@users.noreply.github.com>
2024-03-29 13:18:51 +0000

    Main menu now runs the Calculator!

--- src/MainMenu.py
+++ src/MainMenu.py
@@ -3,9 +3,27 @@
 import tkinter as tk
 from tkinter import *
 import time
+import os
+import subprocess
+import sys
+
+# setting up file paths.
+
+GUI_Calculator = '/PythonProjects/src/Main.py'
+
+args = '"%s" "%s"' % (sys.executable, GUI_Calculator)
+
+
+def openfile():
+    proc = subprocess.run(args)
+    mainMenu.destroy()
+
 
 mainMenu = tk.Tk()
 mainMenu.title("Main menu")
 mainMenu.geometry("400x200")
-mainMenu.resizable()
+welcomeLabel = Label(mainMenu, text="Welcome to the main menu. Please select from the following options")
+welcomeLabel.grid(row=0, column=0)
+openCalculatorButton = Button(mainMenu, text="Open Calculator", command=openfile)
+openCalculatorButton.grid(row=1, column=1)
 mainMenu.mainloop()
